552569b59195311be1c4ce4ebee2924ed63d79b8
Camilo Saldías González <camilo@MacBook-Air-de-Camilo.local>
2017-05-02 23:29:57 -0300

    Agregados comentarios
    Agregué algunos comentarios adicionales. Me falta poner una descripción
    un poco más detallada de la función.

--- SearchRank/SearchRank.py
+++ SearchRank/SearchRank.py
@@ -8,16 +8,21 @@ def searchRank(sites_db, query, user_cat, num_results):
 	#Sites_db es una lista de tuplas (for now) con los datos de los sitios web indexados
 	results = []
 	for (nombre, desc, url, keywords, pond, auth) in sites_db:
+		#Si el contenido no está autorizado, lo omitimos
 		if not auth: continue
 		num_match = 0
 		#Cuantas keywords de la query coinciden con las keywords del archivo?
 		for word in (query.split(" ")):
 			num_match += keywords.count(word)
+
 		#Calculamos el ptje del archivo
 		accuracy = num_match / len(keywords)
 		file_score = accuracy * pond[user_cat]
+
 		results.append( (file_score, nombre, desc, url) )
 
+	#Ordenamos los resultados por puntaje desdendente, de esta forma los resultados más
+	#relevantes para el usuario se mostrarán primero.
 	results.sort(key=lambda x: x[0], reverse=True)
 	return results
 
