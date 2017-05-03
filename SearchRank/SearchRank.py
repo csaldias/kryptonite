7e2170c6e8f1ea6cbcb37d0548e079b75cd728f5
Camilo Saldías González <camilo@MacBook-Air-de-Camilo.local>
2017-05-02 22:59:34 -0300

    Datos de prueba iniciales

---
+++ SearchRank/SearchRank.py
@@ -0,0 +1,13 @@
+from sys import stderr as stderr
+import numpy as np
+'''
+Función searchRank: brinda los principales resultados para el usuario, en orden de relevancia
+
+'''
+def searchRank(sites_db, query, user_cat, num_results):
+	#Sites_db es una lista de tuplas (for now) con los datos de los sitios web indexados
+	for()
+
+
+if __name__ == '__main__':
+	print('Error: This function must be used as a module.', file=stderr)
