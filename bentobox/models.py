39213818ed7e677ac79dbd01a623ef249b482e78
Camilo Saldías <camilo.saldias@gmail.com>
2017-06-12 15:20:59 -0400

    Sistema mejorado de búsqueda
    Ahora es posible realizar búsquedas sobre el contenido almecenado en la
    BD, en base al perfil cognitivo de la persona.

--- bentobox/models.py
+++ bentobox/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Contenido(models.Model):
-    tipos_contenido = (
-    ('sd', 'Sin Definir'),
-    ('ad', 'Adaptador'),
-    ('di', 'Divergente'),
-    ('co', 'Convergente'),
-    ('as', 'Asimilador'),
+    ptje = (
+    (0, 'No relevante'),
+    (1, 'Relevante'),
+    (2, 'Muy relevante'),
     )
+
     link = models.URLField(max_length=1024)
-    desc = models.CharField(max_length=200)
-    #TODO: Faltan los TAG's!
-    clasif = models.CharField(max_length=2,
-                              choices = tipos_contenido,
-                              default = 'sd')
-    valid = models.BooleanField()
+    descripcion = models.CharField(max_length=200)
+    tags = models.CharField(max_length=2048)
+    clasificacion_adaptador = models.IntegerField(choices = ptje,default = 1)
+    clasificacion_divergente = models.IntegerField(choices = ptje,default = 1)
+    clasificacion_convergente = models.IntegerField(choices = ptje,default = 1)
+    clasificacion_asimilador = models.IntegerField(choices = ptje,default = 1)
+    aprobado = models.BooleanField()
 
     def __str__(self):
-        return self.link
+        return self.link+" - "+self.descripcion
--- bentobox/views.py
+++ bentobox/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import Contenido
 def index(request):
     return HttpResponse("Este es el index de bentobox.")
 
 def searchPage(request):
     tipos_contenido = (
-    ('sd', 'Sin Definir'),
     ('ad', 'Adaptador'),
     ('di', 'Divergente'),
     ('co', 'Convergente'),
@@ -17,6 +17,39 @@ def searchPage(request):
     return render(request, 'bentobox/search.html', context)
 
 def searchResults(request):
+    search_query = request.POST["search_query"]
+    tipo_user = request.POST["tipo"]
 
-    context = {'search_query': request.POST["search_query"], 'tipo': request.POST["tipo"]}
+    #Aquí va la lógica de searchRank
+    results = []
+    contenidos = Contenido.objects.filter(aprobado=1)
+    for contenido in contenidos:
+        num_match = 0
+        tags = contenido.tags.split(",")
+        #Cuantas keywords de la query coinciden con las keywords del archivo?
+        for word in (search_query.split(" ")):
+            num_match += tags.count(word)
+
+        #Calculamos el ptje del archivo
+        accuracy = num_match / len(tags)
+        if tipo_user == "ad":
+            file_score = round(accuracy * contenido.clasificacion_adaptador, 3)
+        elif tipo_user == "di":
+            file_score = round(accuracy * contenido.clasificacion_divergente, 3)
+        elif tipo_user == "co":
+            file_score = round(accuracy * contenido.clasificacion_convergente, 3)
+        elif tipo_user == "as":
+            file_score = round(accuracy * contenido.clasificacion_asimilador, 3)
+
+        results.append( (file_score, contenido.link, contenido.descripcion) )
+
+    #Ordenamos los resultados por puntaje desdendente, de esta forma los
+    #resultados másrelevantes para el usuario se mostrarán primero.
+    results.sort(key=lambda x: x[0], reverse=True)
+
+    context = {
+        'search_query': search_query,
+        'tipo': tipo_user,
+        'resultados': results
+        }
     return render(request, 'bentobox/results.html', context)
