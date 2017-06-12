a076e326813260f1ceb51c461ab81189eb79dc57
Camilo Saldías <camilo.saldias@gmail.com>
2017-06-11 23:30:03 -0400

    Agregado modelo para Contenido
    Se añadió el modelo para el Contenido en models.py

--- bentobox/models.py
+++ bentobox/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Contenido(models.Model):
+    tipos_contenido = (
+    ('sd', 'Sin Definir'),
+    ('ad', 'Adaptador'),
+    ('di', 'Divergente'),
+    ('co', 'Convergente'),
+    ('as', 'Asimilador'),
+    )
+    link = models.URLField(max_length=1024)
+    desc = models.CharField(max_length=200)
+    #TODO: Faltan los TAG's
+    clasif = models.CharField(max_length=2,
+                              choices = tipos_contenido,
+                              default = 'sd')
+    valid = models.BooleanField()
